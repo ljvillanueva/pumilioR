a5801fed0d34d432b38d85159854377dce067c56
Luis J. Villanueva <ljvillanueva@coquipr.com>
2013-07-07 11:13:37 -0400

    Cleanup, getting ready for v 1.0 to submit to CRAN.

--- R/getSounds.R
+++ R/getSounds.R
@@ -1,5 +1,6 @@
 getSounds <- function(pumilio_URL, SiteID=NA, ColID=NA, type="all"){
-	
+	#Function to get the info of sound files that match a query, 
+	# or all sounds in archive is no query is used.
 	if (getVersion(pumilio_URL)==FALSE){
 		stop(" pumilioR only works with Pumilio version 2.6.0 or newer.")
 	}
@@ -8,16 +9,14 @@ getSounds <- function(pumilio_URL, SiteID=NA, ColID=NA, type="all"){
 		stop(" Both ColID and SiteID cannot be empty when type is not 'all'.")
 	}
 	
-	#if (is.na(SiteID)==FALSE & is.na(ColID)==FALSE & ()){
-	#	stop(" Only one of ColID and SiteID can be set.")
-	#}
-	
 	pumilio_XML_URL <- paste(pumilio_URL, "xml.php", sep="")
 	
 	this_site_sounds <- xmlTreeParse(paste(pumilio_XML_URL, "?type=", type, "&SiteID=", SiteID, "&ColID=", ColID, sep=""), isURL = TRUE)
 	sounds_list <- xmlToList(node=this_site_sounds, addAttributes=TRUE)
 	
+	#Get sounds from parsed XML
 	sound_list <- sounds_list$Sounds
 	
+	#Return df of sound data
 	invisible(as.data.frame(t(sound_list), row.names=FALSE))
 }
