899494dd4daa2e326e07fa61364e87ea7cc0557f
Luis J. Villanueva <ljvillanueva@coquipr.com>
2014-02-07 19:39:11 -0500

    Adding check for curl in system

--- pumilioR/R/getFile.R
+++ pumilioR/R/getFile.R
@@ -5,15 +5,21 @@ getFile <- function(result, SoundID = NA, credentials = NA, pumiliologin = NA){
 	}
 	
 	if (!is.na(credentials)){
+		#Check if curl is installed
+		if (system("curl -V", ignore.stderr = TRUE)!=0){
+			stop("curl was not found")
+		}
+		
 		soundfilePath <- unlist(result[result$SoundID==SoundID,]$FilePath)
 		soundfilePath <- gsub("http://", paste("http://", credentials, "@", sep=""), soundfilePath)
+		localfile = basename(soundfilePath)
+		download.file(soundfilePath, destfile = localfile, mode="wb", method="curl")
 	}else{
 		soundfilePath <- unlist(result[result$SoundID==SoundID,]$FilePath)
+		localfile = basename(soundfilePath)
+		download.file(soundfilePath, destfile = localfile, mode="wb")
 		}
 	
-	localfile = basename(soundfilePath)
-	download.file(soundfilePath, destfile = localfile, mode="wb", method="curl")
-  
 	#Return df of sound data
 	invisible(localfile)
 }
